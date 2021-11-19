49c92ff41c377e9bae7db07a08e3bb29e1996313
vfryer12 <72739659+vfryer12@users.noreply.github.com>
2021-11-19 15:14:12 +0000

    Fixed toggle

---
+++ js/mainPage.js
@@ -0,0 +1,8 @@
+(function() {(".toggle").on("click", function() {
+        if ((".item").hasClass("active")) {
+            (".item").removeClass("active");
+        } else {
+            (".item").addClass("active");
+        }
+    });
+});
