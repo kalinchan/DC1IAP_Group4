93f631ff209c73cb2f6f11ab3f21ff343ff1f160
Kalin Chan <kalin.chan@payara.fish>
2021-11-19 14:10:49 +0000

    Worked on Database data

--- js/index.js
+++ js/index.js
@@ -1,6 +1,47 @@
-var express = require('express');
-var port = process.env.PORT || 5000;
+const express = require('express');
+const port = process.env.PORT || 5000;
+
+const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("DB Connected"))
+    .catch(err => console.log(err));
+
+const FormSchema = new mongoose.Schema({
+    name: String,
+    occupation: String,
+    company: String,
+    country: String,
+    email: String,
+    context: String,
+    updates: Boolean,
+    contact: Boolean
+});
+
+async function sendData(req, res) {
+    const Interest = mongoose.model('Interest', FormSchema);
+    const document = new Interest({
+        name: (req.body.name),
+        occupation: (req.body.occupation),
+        company: (req.body.company),
+        country: (req.body.country),
+        email: (req.body.email),
+        context: (req.body.context),
+        updates: (req.body.updates === 'true'),
+        contact: (req.body.contact === 'true')
+    });
+    document.markModified('anything');
+    document.save();
+    res.json(document)
+}
 
 express()
     .get('/', (req, res) => res.send('Hello World'))
-    .listen(port, () => console.log(`Listening on ${ port }`))
+    .use(express.static("public"))
+    .use(express.urlencoded({extended: true}))
+    .get('/test', (req, res) => res.sendFile(__dirname + "/test.html"))
+    .post('/post', async (req, res) => {
+        sendData(req, res)
+    })
+    .listen(port, () => console.log(`Listening on ${ port }`));
---
+++ public/ajax.js
@@ -0,0 +1,7 @@
+
+//This will send data to MongoDB
+
+function sendData() {
+    console.log("test");
+
+}
